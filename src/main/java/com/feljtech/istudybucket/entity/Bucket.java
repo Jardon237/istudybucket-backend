8bd64c2d7c0d0527f9ad88ee879bc2ae49969d55
Ngwa Jude <ngwajude161@gmail.com>
2021-09-06 10:08:00 +0100

    bucket entity

---
+++ src/main/java/com/feljtech/istudybucket/entity/Bucket.java
@@ -0,0 +1,10 @@
+package com.feljtech.istudybucket.entity;
+
+public class Bucket {
+    private Long BuckedId;
+    private int number_of_post;
+    private Date creation_date;
+    private String moderations;
+    private Long FieldId;
+    private Long forumId;
+}
