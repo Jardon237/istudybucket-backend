aae1800c8f2d0af0e9301dc5e67ccb5e4bbf5ddc
Ngwa Jude <ngwajude161@gmail.com>
2021-09-06 10:07:17 +0100

    added massage entity

---
+++ src/main/java/com/feljtech/istudybucket/entity/Message.java
@@ -0,0 +1,11 @@
+package com.feljtech.istudybucket.entity;
+
+public  class Message {
+    private Long MessageId;
+    private Long senderId;
+    private Long RecieverId;
+    private Date send_time;
+    private Date recieve_time;
+    private String content;
+    private Long forumId;
+}
