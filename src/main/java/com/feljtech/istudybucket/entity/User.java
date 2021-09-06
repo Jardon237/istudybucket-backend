bd1f8b51f8fa617ee9d931437da84672812a0a10
Ngwa Jude <ngwajude161@gmail.com>
2021-09-06 09:11:31 +0100

    added user entity

--- src/main/java/com/feljtech/istudybucket/entity/User.java
+++ src/main/java/com/feljtech/istudybucket/entity/User.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.persistence.*;
 
-
 /**
  * User
  */
@@ -22,10 +21,14 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
-
     private String username;
-
     private String firstName;
-
     private String lastName;
+    private string email;
+    private string phone_number;
+    private  string  address;
+    private short gender;
+    private  sring password;
+    private Date date_of_birth;
+
 }
