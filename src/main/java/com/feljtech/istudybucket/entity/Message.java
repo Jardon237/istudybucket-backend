df69d16c6a7e2d2c4e571aa729638098b31a388e
Ngwa Jude <ngwajude161@gmail.com>
2021-09-07 19:20:05 +0100

    updated message entity

--- src/main/java/com/feljtech/istudybucket/entity/Message.java
+++ src/main/java/com/feljtech/istudybucket/entity/Message.java
@@ -1,11 +1,8 @@
 package com.feljtech.istudybucket.entity;
 
 public  class Message {
-    private Long MessageId;
-    private Long senderId;
-    private Long RecieverId;
-    private Date send_time;
-    private Date recieve_time;
+    private int MessageId;
+    private int senderId;
     private String content;
-    private Long forumId;
+    private  int chatid;
 }
